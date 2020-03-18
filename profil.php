5c0af15efabeb82f8b2807611749b9b61634dd6b
nirsa <nirsa11@gmail.com>
2020-03-18 14:30:11 +0200

    wip

--- profil.php
+++ profil.php
@@ -5,22 +5,20 @@ include_once 'config/init.php';
 session_start();
 ?>
 <?php
-if($_SESSION['id'] == $user->id || $_SESSION['role'] == 1){
+
     $template = new Template('templates/profil-user.php');
     if(isset($_GET['id'])){
         $id =  $_GET['id'];
         $profil = new Profil('foo');
         $user = new User($id);
         $job = new Job();
+    if($_SESSION['id'] == $user->id || $_SESSION['role'] == 1){
         $template->myProfil = $profil->getProfil($id);
         $template->user = $user->getUser();
         $template->categories = $job->getCategories();
         $template->def = 'default.pdf';
+    }else{
+        header('Location:login.php');
+    }    
     }
-}else{
-    header('Location:login.php');
-}
-
-
-
 echo $template;
